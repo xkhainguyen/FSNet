2360cfd88d28d3885f711f126b209514deb87f51
Khai Nguyen <khai.nx1201@gmail.com>
2025-11-02 16:57:22 -0500

    up

--- results/results.py
+++ results/results.py
@@ -1,6 +1,51 @@
 import numpy as np
 
-# Finetune checkpoint 20 of different dataset sizes with suboptimality 0.0
+fsnet_ft_ckpt20_subopt20_over_train_size = np.array([
+    [
+        "20251101-234017_MLP_FSNet_seed0_dropout0.1_finetune_20251101-233217_model_20",
+        "20251102-003328_MLP_FSNet_seed1_dropout0.1_finetune_20251101-233217_model_20",
+        "20251102-012534_MLP_FSNet_seed2_dropout0.1_finetune_20251101-233217_model_20",
+        "20251102-012156_MLP_FSNet_seed3_dropout0.1_finetune_20251101-233217_model_20",
+    ],
+    [
+        "20251101-235251_MLP_FSNet_seed0_dropout0.1_finetune_20251101-233236_model_20",
+        "20251102-004558_MLP_FSNet_seed1_dropout0.1_finetune_20251101-233236_model_20",
+        "20251102-013943_MLP_FSNet_seed2_dropout0.1_finetune_20251101-233236_model_20",
+        "20251102-013453_MLP_FSNet_seed3_dropout0.1_finetune_20251101-233236_model_20",
+    ],
+    [
+        "20251102-000639_MLP_FSNet_seed0_dropout0.1_finetune_20251101-233303_model_20",
+        "20251102-005943_MLP_FSNet_seed1_dropout0.1_finetune_20251101-233303_model_20",
+        "20251102-015453_MLP_FSNet_seed2_dropout0.1_finetune_20251101-233303_model_20",
+        "20251102-014842_MLP_FSNet_seed3_dropout0.1_finetune_20251101-233303_model_20",
+    ],
+    [
+        "20251102-001926_MLP_FSNet_seed0_dropout0.1_finetune_20251101-233338_model_20",
+        "20251102-011248_MLP_FSNet_seed1_dropout0.1_finetune_20251101-233338_model_20",
+        "20251102-010834_MLP_FSNet_seed2_dropout0.1_finetune_20251101-233338_model_20",
+        "20251102-020113_MLP_FSNet_seed3_dropout0.1_finetune_20251101-233338_model_20",
+    ],
+    [
+        "20251031-130001_MLP_FSNet_seed0_dropout0.1_finetune_20251031-073026_model_20",
+        "20251031-133322_MLP_FSNet_seed1_dropout0.1_finetune_20251031-073026_model_20",
+        "20251031-140451_MLP_FSNet_seed2_dropout0.1_finetune_20251031-073026_model_20",
+        "20251031-143652_MLP_FSNet_seed3_dropout0.1_finetune_20251031-073026_model_20",
+    ], # ckpt20_subopt20_train_size7000
+    [
+        "20251030-234500_MLP_FSNet_seed0_dropout0.1_finetune_20251030-232939_model_20",
+        "20251031-005543_MLP_FSNet_seed1_dropout0.1_finetune_20251030-232939_model_20",
+        "20251031-011200_MLP_FSNet_seed2_dropout0.1_finetune_20251030-232939_model_20",
+        "20251031-012540_MLP_FSNet_seed3_dropout0.1_finetune_20251030-232939_model_20",
+    ], # ckpt20_subopt00_train_size7000
+    [
+        "20251031-203828_MLP_FSNet_seed0_dropout0.1",
+        "20251031-205035_MLP_FSNet_seed1_dropout0.1",
+        "20251031-210247_MLP_FSNet_seed2_dropout0.1",
+        "20251031-211532_MLP_FSNet_seed3_dropout0.1"
+    ],
+]) # matrix of shape (num_ckpt, num_seeds)
+
+# Finetune checkpoint 20 with suboptimality 0.0 over different dataset sizes 
 fsnet_ft_ckpt20_subopt00_over_train_size = np.array([
     [
         "20251031-124440_MLP_FSNet_seed0_dropout0.1_finetune_20251031-075451_model_20",
@@ -13,7 +58,7 @@ fsnet_ft_ckpt20_subopt00_over_train_size = np.array([
         "20251031-134943_MLP_FSNet_seed1_dropout0.1_finetune_20251031-074829_model_20",
         "20251031-144555_MLP_FSNet_seed2_dropout0.1_finetune_20251031-074829_model_20",
         "20251031-154355_MLP_FSNet_seed3_dropout0.1_finetune_20251031-074829_model_20",
-    ],
+    ], #500
     [
         "20251031-130941_MLP_FSNet_seed0_dropout0.1_finetune_20251031-074856_model_20",
         "20251031-140212_MLP_FSNet_seed1_dropout0.1_finetune_20251031-074856_model_20",
@@ -42,6 +87,12 @@ fsnet_ft_ckpt20_subopt00_over_train_size = np.array([
 
 # Finetune checkpoint 20 of different suboptimalities
 fsnet_ft_ckpt20_train_size7000_over_subopt = np.array([
+    [
+        "20251031-203828_MLP_FSNet_seed0_dropout0.1",
+        "20251031-205035_MLP_FSNet_seed1_dropout0.1",
+        "20251031-210247_MLP_FSNet_seed2_dropout0.1",
+        "20251031-211532_MLP_FSNet_seed3_dropout0.1"
+    ],
     [
         "20251030-234500_MLP_FSNet_seed0_dropout0.1_finetune_20251030-232939_model_20",
         "20251031-005543_MLP_FSNet_seed1_dropout0.1_finetune_20251030-232939_model_20",
@@ -99,6 +150,28 @@ fsnet_ft_subopt100_train_size7000_over_ckpt = np.array([
         "20251101-040830_MLP_FSNet_seed1_dropout0.1_finetune_20251101-003437_model_60",
         "20251101-044648_MLP_FSNet_seed2_dropout0.1_finetune_20251101-003437_model_60",
         "20251101-052807_MLP_FSNet_seed3_dropout0.1_finetune_20251101-003437_model_60",
+    ],
+]) # matrix of shape (num_ckpt, num_seeds)
+
+# Suboptimality 6.0 at different checkpoints
+fsnet_ft_subopt60_train_size7000_over_ckpt = np.array([
+    [
+        "20251031-203828_MLP_FSNet_seed0_dropout0.1",
+        "20251031-205035_MLP_FSNet_seed1_dropout0.1",
+        "20251031-210247_MLP_FSNet_seed2_dropout0.1",
+        "20251031-211532_MLP_FSNet_seed3_dropout0.1"
+    ],
+    [
+        "20251101-232533_MLP_FSNet_seed0_dropout0.1_finetune_20251101-232010_model_20",
+        "20251101-234224_MLP_FSNet_seed1_dropout0.1_finetune_20251101-232010_model_20",
+        "20251102-000057_MLP_FSNet_seed2_dropout0.1_finetune_20251101-232010_model_20",
+        "20251102-001948_MLP_FSNet_seed3_dropout0.1_finetune_20251101-232010_model_20",
+    ],
+    [
+        "20251102-003834_MLP_FSNet_seed0_dropout0.1_finetune_20251101-232010_model_60",
+        "20251102-005853_MLP_FSNet_seed1_dropout0.1_finetune_20251101-232010_model_60",
+        "20251102-011918_MLP_FSNet_seed2_dropout0.1_finetune_20251101-232010_model_60",
+        "20251102-013941_MLP_FSNet_seed3_dropout0.1_finetune_20251101-232010_model_60",
     ],
 ]) # matrix of shape (num_ckpt, num_seeds)
 
@@ -259,3 +332,151 @@ fsnet_ft_subopt00_train_size7000_over_ckpt = np.array([
         "20251031-034823_MLP_FSNet_seed3_dropout0.1_finetune_20251030-232939_model_200",
     ]
 ]) # matrix of shape (num_ckpt, num_seeds)
+
+
+dc3_ft_subopt20_train_size7000_over_ckpt = np.array([
+    [
+        "20251102-112406_MLP_DC3_seed0_dropout0.1",
+        "20251102-112606_MLP_DC3_seed1_dropout0.1",
+        "20251102-112806_MLP_DC3_seed2_dropout0.1",
+        "20251102-113006_MLP_DC3_seed3_dropout0.1",
+    ],
+    [
+        "20251031-170438_MLP_DC3_seed0_dropout0.1_finetune_20251031-163616_model_20",
+        "20251031-171212_MLP_DC3_seed1_dropout0.1_finetune_20251031-163616_model_20",
+        "20251031-171947_MLP_DC3_seed2_dropout0.1_finetune_20251031-163616_model_20",
+    ], # 0.0
+    [
+        "20251031-172723_MLP_DC3_seed0_dropout0.1_finetune_20251031-163616_model_60",
+        "20251031-173457_MLP_DC3_seed1_dropout0.1_finetune_20251031-163616_model_60",
+        "20251031-174232_MLP_DC3_seed2_dropout0.1_finetune_20251031-163616_model_60",
+    ],
+    [
+        "20251031-182238_MLP_DC3_seed0_dropout0.1_finetune_20251031-163616_model_100",
+        "20251031-183014_MLP_DC3_seed1_dropout0.1_finetune_20251031-163616_model_100",
+        "20251031-183751_MLP_DC3_seed2_dropout0.1_finetune_20251031-163616_model_100",
+    ],
+    [
+        "20251031-184531_MLP_DC3_seed0_dropout0.1_finetune_20251031-163616_model_200",
+        "20251031-185311_MLP_DC3_seed1_dropout0.1_finetune_20251031-163616_model_200",
+        "20251031-190051_MLP_DC3_seed2_dropout0.1_finetune_20251031-163616_model_200",
+    ],
+])
+
+
+dc3_ft_subopt10_train_size7000_over_ckpt = np.array([
+    [
+        "20251102-112406_MLP_DC3_seed0_dropout0.1",
+        "20251102-112606_MLP_DC3_seed1_dropout0.1",
+        "20251102-112806_MLP_DC3_seed2_dropout0.1",
+        "20251102-113006_MLP_DC3_seed3_dropout0.1",
+    ],
+    [
+        "20251031-170244_MLP_DC3_seed0_dropout0.1_finetune_20251031-163247_model_20",
+        "20251031-171019_MLP_DC3_seed1_dropout0.1_finetune_20251031-163247_model_20",
+        "20251031-171754_MLP_DC3_seed2_dropout0.1_finetune_20251031-163247_model_20",
+    ], # 0.0
+    [
+        "20251031-172529_MLP_DC3_seed0_dropout0.1_finetune_20251031-163247_model_60",
+        "20251031-173304_MLP_DC3_seed1_dropout0.1_finetune_20251031-163247_model_60",
+        "20251031-174038_MLP_DC3_seed2_dropout0.1_finetune_20251031-163247_model_60",
+    ],
+    [
+        "20251031-182044_MLP_DC3_seed0_dropout0.1_finetune_20251031-163247_model_100",
+        "20251031-182820_MLP_DC3_seed1_dropout0.1_finetune_20251031-163247_model_100",
+        "20251031-183556_MLP_DC3_seed2_dropout0.1_finetune_20251031-163247_model_100",
+    ],
+    [
+        "20251031-184336_MLP_DC3_seed0_dropout0.1_finetune_20251031-163247_model_200",
+        "20251031-185116_MLP_DC3_seed1_dropout0.1_finetune_20251031-163247_model_200",
+        "20251031-185856_MLP_DC3_seed2_dropout0.1_finetune_20251031-163247_model_200",
+    ],
+])
+
+
+dc3_ft_subopt05_train_size7000_over_ckpt = np.array([
+    [
+        "20251102-112406_MLP_DC3_seed0_dropout0.1",
+        "20251102-112606_MLP_DC3_seed1_dropout0.1",
+        "20251102-112806_MLP_DC3_seed2_dropout0.1",
+        "20251102-113006_MLP_DC3_seed3_dropout0.1",
+    ],
+    [
+        "20251031-170049_MLP_DC3_seed0_dropout0.1_finetune_20251031-163030_model_20",
+        "20251031-170825_MLP_DC3_seed1_dropout0.1_finetune_20251031-163030_model_20",
+        "20251031-171601_MLP_DC3_seed2_dropout0.1_finetune_20251031-163030_model_20",
+    ], # 0.0
+    [
+        "20251031-172335_MLP_DC3_seed0_dropout0.1_finetune_20251031-163030_model_60",
+        "20251031-173110_MLP_DC3_seed1_dropout0.1_finetune_20251031-163030_model_60",
+        "20251031-173845_MLP_DC3_seed2_dropout0.1_finetune_20251031-163030_model_60",
+    ],
+    [
+        "20251031-181851_MLP_DC3_seed0_dropout0.1_finetune_20251031-163030_model_100",
+        "20251031-182626_MLP_DC3_seed1_dropout0.1_finetune_20251031-163030_model_100",
+        "20251031-183401_MLP_DC3_seed2_dropout0.1_finetune_20251031-163030_model_100",
+    ],
+    [
+        "20251031-184140_MLP_DC3_seed0_dropout0.1_finetune_20251031-163030_model_200",
+        "20251031-184921_MLP_DC3_seed1_dropout0.1_finetune_20251031-163030_model_200",
+        "20251031-185701_MLP_DC3_seed2_dropout0.1_finetune_20251031-163030_model_200",
+    ],
+])
+
+dc3_ft_subopt00_train_size7000_over_ckpt = np.array([
+    [
+        "20251102-112406_MLP_DC3_seed0_dropout0.1",
+        "20251102-112606_MLP_DC3_seed1_dropout0.1",
+        "20251102-112806_MLP_DC3_seed2_dropout0.1",
+        "20251102-113006_MLP_DC3_seed3_dropout0.1",
+    ],
+    [
+        "20251031-165855_MLP_DC3_seed0_dropout0.1_finetune_20251031-162815_model_20",
+        "20251031-170631_MLP_DC3_seed1_dropout0.1_finetune_20251031-162815_model_20",
+        "20251031-171407_MLP_DC3_seed2_dropout0.1_finetune_20251031-162815_model_20",
+    ], # 0.0
+    [
+        "20251031-172141_MLP_DC3_seed0_dropout0.1_finetune_20251031-162815_model_60",
+        "20251031-172917_MLP_DC3_seed1_dropout0.1_finetune_20251031-162815_model_60",
+        "20251031-173651_MLP_DC3_seed2_dropout0.1_finetune_20251031-162815_model_60",
+    ],
+    [
+        "20251031-181657_MLP_DC3_seed0_dropout0.1_finetune_20251031-162815_model_100",
+        "20251031-182432_MLP_DC3_seed1_dropout0.1_finetune_20251031-162815_model_100",
+        "20251031-183207_MLP_DC3_seed2_dropout0.1_finetune_20251031-162815_model_100",
+    ],
+    [
+        "20251031-183946_MLP_DC3_seed0_dropout0.1_finetune_20251031-162815_model_200",
+        "20251031-184726_MLP_DC3_seed1_dropout0.1_finetune_20251031-162815_model_200",
+        "20251031-185506_MLP_DC3_seed2_dropout0.1_finetune_20251031-162815_model_200",
+    ],
+])
+
+dc3_ft_ckpt20_train_size7000_over_subopt = np.array([
+    [
+        "20251102-112406_MLP_DC3_seed0_dropout0.1",
+        "20251102-112606_MLP_DC3_seed1_dropout0.1",
+        "20251102-112806_MLP_DC3_seed2_dropout0.1",
+        "20251102-113006_MLP_DC3_seed3_dropout0.1",
+    ],
+    [
+        "20251031-165855_MLP_DC3_seed0_dropout0.1_finetune_20251031-162815_model_20",
+        "20251031-170631_MLP_DC3_seed1_dropout0.1_finetune_20251031-162815_model_20",
+        "20251031-171407_MLP_DC3_seed2_dropout0.1_finetune_20251031-162815_model_20",
+    ], # 0.0
+    [
+        "20251031-170049_MLP_DC3_seed0_dropout0.1_finetune_20251031-163030_model_20",
+        "20251031-170825_MLP_DC3_seed1_dropout0.1_finetune_20251031-163030_model_20",
+        "20251031-171601_MLP_DC3_seed2_dropout0.1_finetune_20251031-163030_model_20",
+    ],
+    [
+        "20251031-170244_MLP_DC3_seed0_dropout0.1_finetune_20251031-163247_model_20",
+        "20251031-171019_MLP_DC3_seed1_dropout0.1_finetune_20251031-163247_model_20",
+        "20251031-171754_MLP_DC3_seed2_dropout0.1_finetune_20251031-163247_model_20",
+    ],
+    [
+        "20251031-170438_MLP_DC3_seed0_dropout0.1_finetune_20251031-163616_model_20",
+        "20251031-171212_MLP_DC3_seed1_dropout0.1_finetune_20251031-163616_model_20",
+        "20251031-171947_MLP_DC3_seed2_dropout0.1_finetune_20251031-163616_model_20",
+    ],
+])
--- sweep_train_size.sh
+++ sweep_train_size.sh
@@ -5,77 +5,78 @@
 # source ~/.bashrc && conda activate ml4opt
 # cd FSNet/
 
-# for seed in 0 1 2 3; do
-#     python main.py \
-#         --method FSNet \
-#         --prob_type nonsmooth_nonconvex \
-#         --prob_name socp \
-#         --seed $seed \
-#         --num_epochs 300 \
-#         --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251031-075451_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 0.0
-#     # Draw something funny
-#     echo "============================================================"
-#     echo "==========  200       =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "============================================================"
-#     python main.py \
-#         --method FSNet \
-#         --prob_type nonsmooth_nonconvex \
-#         --prob_name socp \
-#         --seed $seed \
-#         --num_epochs 300 \
-#         --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251031-074829_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 0.0
-#     # Draw something funny
-#     echo "============================================================"
-#     echo "==========  500       =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "============================================================"
-#     python main.py \
-#         --method FSNet \
-#         --prob_type nonsmooth_nonconvex \
-#         --prob_name socp \
-#         --seed $seed \
-#         --num_epochs 300 \
-#         --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251031-074856_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 0.0
-#     # Draw something funny
-#     echo "============================================================"
-#     echo "==========  1000       =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "============================================================"
-#     python main.py \
-#         --method FSNet \
-#         --prob_type nonsmooth_nonconvex \
-#         --prob_name socp \
-#         --seed $seed \
-#         --num_epochs 300 \
-#         --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251031-074930_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 0.0
-#     # Draw something funny
-#     echo "============================================================"
-#     echo "==========  4000       =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "==========         =====================         ==========="
-#     echo "============================================================"
-# done
-
-for size in 200, 500, 1000, 4000; do
+for seed in 0 1 2 3; do
     python main.py \
-        --method sup \
+        --method FSNet \
         --prob_type nonsmooth_nonconvex \
         --prob_name socp \
-        --dropout 0.1 \
-        --lr 0.0005  \
-        --train_size 7000 \
-        --seed 1 \
-        --en_subopt True \
-        --subopt_ratio  2.0
-
+        --seed $seed \
+        --num_epochs 300 \
+        --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251101-233217_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 2.0
+    # Draw something funny
+    echo "============================================================"
+    echo "==========  200       =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "============================================================"
+    python main.py \
+        --method FSNet \
+        --prob_type nonsmooth_nonconvex \
+        --prob_name socp \
+        --seed $seed \
+        --num_epochs 300 \
+        --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251101-233236_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 2.0
+    # Draw something funny
+    echo "============================================================"
+    echo "==========  500       =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "============================================================"
+    python main.py \
+        --method FSNet \
+        --prob_type nonsmooth_nonconvex \
+        --prob_name socp \
+        --seed $seed \
+        --num_epochs 300 \
+        --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251101-233303_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 2.0
+    # Draw something funny
+    echo "============================================================"
+    echo "==========  1000       =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "==========         =====================         ==========="
+    echo "============================================================"
+    python main.py \
+        --method FSNet \
+        --prob_type nonsmooth_nonconvex \
+        --prob_name socp \
+        --seed $seed \
+        --num_epochs 300 \
+        --checkpoint "results/nonsmooth_nonconvex/socp/SOCPProblem-100-50-50-10000/20251101-233338_MLP_sup_seed1_dropout0.1/model_20.pt" # subopt 2.0
     # Draw something funny
     echo "============================================================"
     echo "==========  4000       =====================         ==========="
     echo "==========         =====================         ==========="
     echo "==========         =====================         ==========="
     echo "============================================================"
+
 done
+
+# for size in 200 500 1000 4000; do
+#     python main.py \
+#         --method sup \
+#         --prob_type nonsmooth_nonconvex \
+#         --prob_name socp \
+#         --dropout 0.1 \
+#         --lr 0.0005  \
+#         --train_size $size \
+#         --seed 1 \
+#         --en_subopt True \
+#         --subopt_ratio  2.0
+
+#     # Draw something funny
+#     echo "============================================================"
+#     echo "==========         =====================         ==========="
+#     echo "==========         =====================         ==========="
+#     echo "==========         =====================         ==========="
+#     echo "============================================================"
+# done
