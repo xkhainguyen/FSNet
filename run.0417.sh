b599ac465b114c2582df48b23bcbdf7299804737
Khai Nguyen <khai.nx1201@gmail.com>
2026-04-17 18:31:16 -0400

    Refactor experiment configurations and update result comparisons
    - Updated execution counts in Jupyter notebooks to reflect new runs.
    - Modified result paths in `compare_result.ipynb` to align with new FSNet experiments.
    - Enhanced `default.yaml` configuration with additional parameters for SampledContextMLP and MoE.
    - Revised `run.0417.sh` script to execute FSNet experiments with updated seed ranges and parameters.
    - Adjusted `show_main_table.ipynb` to include new FSNet results and updated metrics display format.

--- run.0417.sh
+++ run.0417.sh
@@ -17,24 +17,7 @@ conda activate ml4opt
 cd /home/khain/orcd/scratch/FSNet
 
 
-for seed in 0 1; do
-    echo "=============================================="
-    echo " Job started at: $(date '+%Y-%m-%d %H:%M:%S')"
-    echo " Job ID: $SLURM_JOB_ID"
-    echo " Node: $SLURM_NODELIST"
-    echo "=============================================="
-
-    python main.py \
-    --seed $seed \
-    --method penalty \
-    --prob_type nonsmooth_nonconvex \
-    --prob_name socp \
-    --network LocalContextMLPv2 \
-    --hidden_dim 2048
-
-done
-
-# for seed in 0 1; do
+# for seed in 0 1 2 3; do
 #     echo "=============================================="
 #     echo " Job started at: $(date '+%Y-%m-%d %H:%M:%S')"
 #     echo " Job ID: $SLURM_JOB_ID"
@@ -43,11 +26,28 @@ done
 
 #     python main.py \
 #     --seed $seed \
-#     --method penalty \
+#     --method FSNet \
 #     --prob_type nonsmooth_nonconvex \
 #     --prob_name socp \
-#     --network MLP \
-#     --hidden_dim 512
+#     --network LocalContextMLPv2 \
+#     --hidden_dim 1024
 
 # done
+
+for seed in 1 2 3; do
+    echo "=============================================="
+    echo " Job started at: $(date '+%Y-%m-%d %H:%M:%S')"
+    echo " Job ID: $SLURM_JOB_ID"
+    echo " Node: $SLURM_NODELIST"
+    echo "=============================================="
+
+    python main.py \
+    --seed $seed \
+    --method FSNet \
+    --prob_type nonsmooth_nonconvex \
+    --prob_name socp \
+    --network MLP \
+    --hidden_dim 512
+
+done
 
